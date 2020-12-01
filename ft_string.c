28038069c7fda57b155c0f9bad445495361b29d8
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2020-12-01 16:25:38 +0100

    ok

--- ft_string.c
+++ ft_string.c
@@ -6,7 +6,7 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 09:24:32 by trouchon          #+#    #+#             */
-/*   Updated: 2020/12/01 16:21:05 by trouchon         ###   ########.fr       */
+/*   Updated: 2020/12/01 16:24:30 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,21 @@
 void	ft_string(t_datas *datas)
 {
 	int i;
+	int k;
 
 	i = 0;
+	k = 0;
 	datas->str = va_arg(datas->ap, char *);
 	while (datas->str[i])
 		i++;
 	if (datas->left_aligned == 1)
 		write(1, datas->str, i);
 	while (datas -> width && datas->width-- > i)
+	{
 		write(1, " ", 1);
+		k = 0;
+	}
 	if (datas->left_aligned == 0)
 		write(1, datas->str, i);
-	datas->chainelen += i - 1;
+	datas->chainelen += i + k - 1;
 }
