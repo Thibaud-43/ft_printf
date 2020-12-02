8c08dd026825e0ac68be6867bb2693a1b6a5f5ca
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2020-12-02 11:15:08 +0100

    ok

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 10:49:29 by trouchon          #+#    #+#             */
-/*   Updated: 2020/12/02 11:12:50 by trouchon         ###   ########.fr       */
+/*   Updated: 2020/12/02 11:15:06 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ void		ft_initializer(t_datas *datas)
 
 void	ft_flags(t_datas *datas)
 {
+	if (STR() == '0')
+	{
+		datas->zero_activated = 1;
+		NEXT();
+	}
 	while (STR() != 's' && STR() != 'c' &&	STR() != 'd')
 	{
-		if (STR() == '0')
-		{
-			datas->zero_activated = 1;
-			NEXT();
-		}
-		while (ft_isdigit(STR()) == 1 && STR() != '0')
+				while (ft_isdigit(STR()) == 1 && STR() != '0')
 		{
 			datas->width = datas->width * 10 + (STR() - 48);
 			NEXT();
