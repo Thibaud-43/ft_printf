546c7b9c61825a648ed16b7efce2f54172e064f8
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2020-12-02 10:24:40 +0100

    ok

--- ft_string.c
+++ ft_string.c
@@ -6,7 +6,7 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 09:24:32 by trouchon          #+#    #+#             */
-/*   Updated: 2020/12/02 10:06:15 by trouchon         ###   ########.fr       */
+/*   Updated: 2020/12/02 10:23:37 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ void	ft_string(t_datas *datas)
 
 	datas->chainelen--;
 	datas->str = va_arg(datas->ap, char *);
-	if (!datas->str && datas->width == 0)
+	if (!datas->str)
+		i = 6;
+	if (!datas->str && datas->left_aligned == 1)
 	{
 		write(1, "(null)", 6);
 		datas->chainelen += 6;
-		return ;
 	}
 	while (datas->str && datas->str[i] && datas->precision > 0)
 	{
@@ -41,6 +42,12 @@ void	ft_string(t_datas *datas)
 		write(1, " ", 1);
 		k++;
 	}
+	if (!datas->str && datas->left_aligned == 0)
+	{
+		write(1, "(null)", 6);
+		datas->chainelen += 6;
+	}
+
 	if (datas->str && datas->left_aligned == 0)
 		write(1, datas->str, i);
 	datas->chainelen += i + k;
--- main.c
+++ main.c
@@ -8,11 +8,11 @@ int main(int argc, char **argv)
 		int ret2;
 
         ft_printf("[MON PRINTF]\n");
-		ret1 = ft_printf("%s %.0s \n", argv[1], argv[2]);
+		ret1 = ft_printf("%s %s \n", argv[1], argv[2]);
       	printf("OUTPUT [%d]\n", ret1);
 
         printf("\n\n[PRINTF]\n");
-		ret2 = printf("%s %.0s \n", argv[1], argv[2]);
+		ret2 = printf("%s %s \n", argv[1], argv[2]);
       	printf("OUTPUT [%d]\n", ret2);
 		return 1;
 }
