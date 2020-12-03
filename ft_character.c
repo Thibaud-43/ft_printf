beecf8949e5310b6019decb516cdeb2239faec9b
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2020-12-03 14:22:51 +0100

    BEGIN CHARACTER

--- ft_character.c
+++ ft_character.c
@@ -6,7 +6,7 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 14:03:29 by trouchon          #+#    #+#             */
-/*   Updated: 2020/12/03 14:12:24 by trouchon         ###   ########.fr       */
+/*   Updated: 2020/12/03 14:22:03 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,21 @@
 
 void		ft_character(t_datas *datas)
 {
+	int i;
+	
+	i = 0;
 	datas->chainelen--;
 	datas->c = va_arg(datas->ap, int);
-	write(1, &datas->c, 1);
-	datas->chainelen++;
+	if (datas->left_aligned == 1)
+		write(1, &datas->c, 1);
+	while (i < datas->width)
+	{
+		write(1, " ", 1);
+		i++;
+	}
+	if (datas->left_aligned == 0)
+		write(1, &datas->c, 1);
+	if (datas->c && datas->width == 0)
+		i = 1;
+	datas->chainelen += (i);
 }
--- main.c
+++ main.c
@@ -8,11 +8,11 @@ int main(int argc, char **argv)
 		int ret2;
 
         ft_printf("[MON PRINTF]\n");
-		ret1 = ft_printf("%c",  atoi(argv[1]));
+		ret1 = ft_printf("%.3c",  atoi(argv[1]));
       	printf("\nOUTPUT [%d]\n", ret1);
 
         printf("\n\n[PRINTF]\n");
-		ret2 = printf("%c", atoi(argv[1]));
+		ret2 = printf("%.3c", atoi(argv[1]));
       	printf("\nOUTPUT [%d]\n", ret2);
 		return 1;
 }
