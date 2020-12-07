e03978e0512d2f3cc3a8375f5951cebacb285eab
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2020-12-07 11:23:02 +0100

    BEGINNING POINTERS

--- ft_pointers.c
+++ ft_pointers.c
@@ -6,7 +6,7 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 17:53:03 by trouchon          #+#    #+#             */
-/*   Updated: 2020/12/07 11:20:33 by trouchon         ###   ########.fr       */
+/*   Updated: 2020/12/07 11:22:55 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ void		ft_pointers(t_datas *datas)
 	z = 0;
 	datas->chainelen--;
 	nb = va_arg(datas->ap, unsigned long);
-	if (nb == 0)
+	if (nb == 0 && datas->precision_len < 3)
 	{
 		ft_zeros(datas, k, i, z);
 		return ;
