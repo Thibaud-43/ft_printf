3b228dd5e82633bd98c933d375239268999a3a8e
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2020-12-07 10:55:24 +0100

    BEGINNING POINTERS

--- ft_pointers.c
+++ ft_pointers.c
@@ -6,7 +6,7 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 17:53:03 by trouchon          #+#    #+#             */
-/*   Updated: 2020/12/07 10:47:23 by trouchon         ###   ########.fr       */
+/*   Updated: 2020/12/07 10:53:47 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ static void		ft_zeros(t_datas *datas, int i, int k, int z)
 	k = 2;
 	if (datas->left_aligned)
 	{
-		write(1, "0x0", 3);
-		while (datas->precision && z < datas->precision_len - 3)
+		write(1, "0x", 3);
+		while (datas->precision && z < datas->precision_len - k)
 		{
 			write(1, "0", 1);
 			z++;
@@ -34,8 +34,8 @@ static void		ft_zeros(t_datas *datas, int i, int k, int z)
 	}
 	if (!datas->left_aligned)
 	{
-		write(1, "0x0", 3);
-		while (datas->precision && z < datas->precision_len - 3)
+		write(1, "0x", 3);
+		while (datas->precision && z < datas->precision_len - k)
 		{
 			write(1, "0", 1);
 			z++;
