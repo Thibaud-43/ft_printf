9b441057d663b7301b24ec8c2fd11c7a68f7b1ba
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2020-12-07 14:30:28 +0100

    BEGINNING x & X

--- ft_hexadecimal.c
+++ ft_hexadecimal.c
@@ -6,7 +6,7 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 13:26:23 by trouchon          #+#    #+#             */
-/*   Updated: 2020/12/07 14:27:14 by trouchon         ###   ########.fr       */
+/*   Updated: 2020/12/07 14:30:25 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ void		ft_hexadecimal(t_datas *datas, int majuscule)
 		datas->decimal = ft_itoa_hexadecimal((unsigned int)nb, "0123456789abcdef");
 	while(datas->decimal[k])
 		k++;
-	if (datas->decimal[0] == '0' && datas->decimal[1] == 0)
-		k = 0;
+	/*if (datas->decimal[0] == '0' && datas->decimal[1] == 0)
+		k = 0;*/
 	if (datas->left_aligned)
 	{
 		tmp = k;
