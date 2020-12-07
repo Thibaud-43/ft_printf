55faee386aa7182e0f03d6b49ac5cd5590cf5588
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2020-12-07 14:12:16 +0100

    BEGINNING x & X

--- main.c
+++ main.c
@@ -7,10 +7,10 @@ int main(int argc, char **argv)
 		int ret1;
 		int ret2;
         ft_printf("[MON PRINTF]\n");
-		ret1 = ft_printf("%-50X", atoi(argv[1]));
+		ret1 = ft_printf("%.5X", atoi(argv[1]));
       	printf("\nOUTPUT [%d]\n", ret1);
         printf("\n\n[PRINTF]\n");
-		ret2 = printf("%-50X", atoi(argv[1]));
+		ret2 = printf("%.5X", atoi(argv[1]));
       	printf("\nOUTPUT [%d]\n", ret2);
 		return 1;
 }
